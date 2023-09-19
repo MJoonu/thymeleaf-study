871db2359ed489fb67739d51f61e334bbe1a3aa9
MJoonu <wnsdn8009@gmail.com>
2023-09-19 18:17:42 +0900

    :tada: Initial commit

---
+++ src/test/java/basic/thymeleafstudy/ThymeleafStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.thymeleafstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
