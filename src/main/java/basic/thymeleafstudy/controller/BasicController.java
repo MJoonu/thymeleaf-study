6a41dc4e6d11dbeb06fed62c43f6590669cf7597
MJoonu <wnsdn8009@gmail.com>
2023-09-22 09:50:45 +0900

    string literal

--- src/main/java/basic/thymeleafstudy/controller/BasicController.java
+++ src/main/java/basic/thymeleafstudy/controller/BasicController.java
@@ -87,6 +87,13 @@ public class BasicController {
         model.addAttribute("param2", "data2");
         return "basic/link";
     }
+    // url link end
+
+    @GetMapping("literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
 
     @Data
     static class User {
